c7089f2831edd6e838ad2e1e2c7f19a48f32bbfe
Anup Kumar Jana <91030891+CoderA78@users.noreply.github.com>
2023-05-05 19:50:24 +0530

    Add files via upload

---
+++ if_else.js
@@ -0,0 +1,17 @@
+let studentScore=101
+
+if (studentScore>=90 && studentScore<=100){
+    console.log("A")
+}
+else if (studentScore>100){
+    console.log("This is an invalid input.")
+}
+else if (studentScore>=89 && studentScore<=70){
+    console.log("B")
+}
+else if (studentScore>=50 && studentScore<=69){
+    console.log("C")
+}
+else{
+    console.log("the student got failed.")
+}
