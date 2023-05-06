5227df561251d4b5b9378f931d55e7a92c0151e0
Anup Kumar Jana <91030891+CoderA78@users.noreply.github.com>
2023-05-06 11:54:09 +0530

    Add files via upload

---
+++ 12letVSVar__const.js
@@ -0,0 +1,23 @@
+var a=30
+
+var a=35
+
+console.log(a)
+
+let b=30
+
+// let b=45
+console.log(b) // this will throw an error because we can't re-declare let keyword value
+
+let c=40
+
+c=45   
+console.log(c)  // in let keyword re-assigning is allowed
+
+
+const d=100
+
+// const d=15   // in const re-declaration is not allowed
+
+d=56 // in const re-assigning is not allowed   
+console.log(d) // this will throw an error 
