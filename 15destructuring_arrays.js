9e6f91bf64b1a076499189c627bacd08b4a9a7d6
Anup Kumar Jana <91030891+CoderA78@users.noreply.github.com>
2023-05-07 20:31:44 +0530

    Add files via upload

---
+++ 15destructuring_arrays.js
@@ -0,0 +1,10 @@
+// destructuring arrays
+
+let arr=['Hi','I','am', 'Anup']
+
+let [a, b, c, d] =arr   // this is called destructuring arrays
+
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
