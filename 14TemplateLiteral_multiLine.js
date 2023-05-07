b5e462b171d07646b5119d5d5a8b27fc8a80be05
Anup Kumar Jana <91030891+CoderA78@users.noreply.github.com>
2023-05-07 19:46:00 +0530

    Add files via upload

---
+++ 14TemplateLiteral_multiLine.js
@@ -0,0 +1,9 @@
+//  ` ` is called back ticks, which can prinrt multi line strings
+console.log(`Welcome    
+to
+my
+PC.`)
+
+let a=24
+
+console.log(`The value of a is: ${a}`)     // this ${ } is called as Template Literal
