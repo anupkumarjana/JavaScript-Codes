e574934c5a97af37cee2be50089f8e8aa1b2beee
Anup Kumar Jana <91030891+CoderA78@users.noreply.github.com>
2023-05-08 11:23:03 +0530

    Add files via upload

---
+++ 18Main_module.js
@@ -0,0 +1,10 @@
+const calculator = require("./17Calculator_module")  // this is the way we can import module from other js files
+
+calculator.addition(2,3)
+
+calculator.substraction(2,3)
+
+calculator.multiplication(2,3)
+
+calculator.division(2,3)
+
