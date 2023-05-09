a37013c5f8b5de0c75e3388af87431c970118bde
Anup Kumar Jana <91030891+CoderA78@users.noreply.github.com>
2023-05-09 21:25:01 +0530

    Add files via upload

---
+++ 20fuction_in_objects.js
@@ -0,0 +1,18 @@
+let details={
+    firstName : "Anup",
+    lastName: "Jana",
+    age: 24, 
+    country : "India",
+    greet : function(){
+         return "Hello! I'm Anup Kumar Jana"
+    }
+
+}
+
+// console.log(details.greet.greeting())
+console.log(details.lastName)
+console.log(details.age)
+console.log(details.country)
+console.log(details.greet())
+
+
