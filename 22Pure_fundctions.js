c6ba067a7cd0624d085c1f34ccf5968785784a65
Anup Kumar Jana <91030891+CoderA78@users.noreply.github.com>
2023-05-12 11:09:42 +0530

    Add files via upload

---
+++ 22Pure_fundctions.js
@@ -0,0 +1,16 @@
+// Pure functions
+
+
+function add(x,a){
+    return x+a
+}
+
+
+
+console.log(add(2,3))
+console.log(add(2,3))
+console.log(add(2,3))
+
+// as many times we can print the fuction but the putput is same.
+// and we also didn't use console.log() in the function, which is an external factor.
+// that's why its an PURE FUNCTION
